9ad8df566c1893e703b5b906701148c106292bc0
Richard Walker <richard.walker.90@gmail.com>
2018-05-21 23:45:49 -0400

    Add double and string assertJ assertions

--- src/test/java/io.richie/AssertJTests.java
+++ src/test/java/io.richie/AssertJTests.java
@@ -1,14 +1,56 @@
 package io.richie;
 
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AssertJTests{
 
+    private Contribution richieContribution;
+    private Contribution richieSecondContribution;
+    private Contribution jamesContribution;
+
+    @BeforeEach
+    void setupTests(){
+        richieContribution = new Contribution(1000.00, "Richie");
+        jamesContribution = new Contribution(2000.00, "James");
+        richieSecondContribution = new Contribution(3000.00, "Richie");
+    }
+
+    @Test
+    void objectAssertions(){
+        assertThat(richieContribution).isEqualToComparingFieldByFieldRecursively(richieSecondContribution)
+                                      .isNotEqualTo(richieSecondContribution)
+                                      .isNotEqualTo(jamesContribution);
+    }
+
     @Test
     void stringAssertions(){
-        assertThat("Foo").isNotBlank();
+        String contributorName = richieContribution.getContributor();
+        String amountAsString = String.valueOf(jamesContribution.getAmount())
+                                      .replace(".","");
+
+        assertThat(contributorName).isNotBlank()
+                                   .containsIgnoringCase("chi")
+                                   .doesNotContain("JAMES")
+                                   .isEqualToIgnoringWhitespace("R i c h i e")
+                                   .isEqualToIgnoringCase("RICHIE");
+
+        assertThat(amountAsString).containsOnlyDigits();
+    }
+
+    @Test
+    void doubleAssertions(){
+        double richieMoney = richieContribution.getAmount();
+        double richieSecondMoney = richieSecondContribution.getAmount();
+        double jamesMoney = jamesContribution.getAmount();
+
+        assertThat(jamesMoney).isGreaterThanOrEqualTo(richieMoney)
+                              .isBetween(richieMoney, richieSecondMoney)
+                              .isCloseTo(richieSecondMoney, Offset.<Double>offset(1000.00))
+                              .isCloseTo(richieSecondMoney, Offset.<Double>strictOffset(1000.01));
     }
 
 }
